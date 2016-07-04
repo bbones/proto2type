0c5c04aa73c1c655c95e5e25d6f53024dc801b55
bbones <boneswilliam@gmail.com>
2016-07-04 22:32:16 +0300

    UI jQuery tuning

--- app/controllers/accounting.js
+++ app/controllers/accounting.js
@@ -1,13 +1,18 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Controller.extend({
 	balanceDetails: false,
 	accountList: false,
-	actions:{
+	actions : {
 		changeBalanceDetail: function(){
 			this.toggleProperty('balanceDetails');
+			debugger;
+			if (this.balanceDetails) {$("#btnChngBlDt").html(' - ');}
+			else {$("#btnChngBlDt").html(' + ');}
+
 		},
-		
+
 		changeAccountList: function() {
 			this.toggleProperty('accountList');
 		}
